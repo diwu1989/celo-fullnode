3ecb9e5d12f8fa732724beb4ed0ffcff25c1bc45
Di Wu <dw323@cornell.edu>
2021-11-26 00:09:35 -0800

    f

--- prune.sh
+++ prune.sh
@@ -4,5 +4,5 @@ docker rm celo-fullnode
 export CELO_IMAGE=us.gcr.io/celo-org/geth:mainnet
 docker run --rm -it --name celo-fullnode-prune \
         --stop-timeout 30 \
-        -v $PWD:/root/.celo export CELO_IMAGE=us.gcr.io/celo-org/geth:mainnet \
+        -v $PWD:/root/.celo $CELO_IMAGE \
         --datadir /root/.celo --nousb removedb
--- start.sh
+++ start.sh
@@ -4,15 +4,16 @@ docker rm celo-fullnode
 
 export CELO_ACCOUNT_ADDRESS=0x5Ca621B88f8f3919eb4B9324CC780a3DF34f95fD
 export CELO_IMAGE=us.gcr.io/celo-org/geth:mainnet
-export SYNC_MODE=ultralight
+export SYNC_MODE=fast
 export MAX_PEERS=32
+export CACHE=128
 docker run --name celo-fullnode -d \
         --restart unless-stopped \
         --stop-timeout 30 \
         -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp \
         -v $PWD:/root/.celo $CELO_IMAGE \
-        --syncmode $SYNC_MODE --rpc --rpcaddr 0.0.0.0 --maxpeers $MAX_PEERS \
+        --syncmode $SYNC_MODE --http --http.addr 0.0.0.0 --maxpeers $MAX_PEERS \
         --etherbase $CELO_ACCOUNT_ADDRESS \
-        --datadir /root/.celo --nousb --cache 64 --snapshot=false \
-        --ws --wsaddr 0.0.0.0 --rpcvhosts '*' --wsorigins '*' \
-        --wsapi eth,net,web3,txpool --rpcapi eth,net,web3,txpool
+        --datadir /root/.celo --nousb --cache $CACHE --snapshot=false \
+        --ws --ws.addr 0.0.0.0 --http.vhosts '*' --ws.origins '*' \
+        --ws.api eth,net,web3,txpool --http.api eth,net,web3,txpool
