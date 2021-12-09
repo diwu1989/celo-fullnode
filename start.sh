b55120face8aa43627d61946bc638f8b49c369ab
Di Wu <dw323@cornell.edu>
2021-12-08 20:25:37 -0800

    f

--- start.sh
+++ start.sh
@@ -4,16 +4,17 @@ docker rm celo-fullnode
 
 export CELO_ACCOUNT_ADDRESS=0x5Ca621B88f8f3919eb4B9324CC780a3DF34f95fD
 export CELO_IMAGE=us.gcr.io/celo-org/geth:mainnet
-export SYNC_MODE=fast
-export MAX_PEERS=32
-export CACHE=128
+export SYNC_MODE=full
+export MAX_PEERS=100
+export CACHE=1024
+export TX_LOOKUP_LIMIT=4320
 docker run --name celo-fullnode -d \
         --restart unless-stopped \
         --stop-timeout 30 \
         -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp \
         -v $PWD:/root/.celo $CELO_IMAGE \
-        --syncmode $SYNC_MODE --http --http.addr 0.0.0.0 --maxpeers $MAX_PEERS \
-        --etherbase $CELO_ACCOUNT_ADDRESS \
-        --datadir /root/.celo --nousb --cache $CACHE --snapshot=false \
+        --syncmode $SYNC_MODE --http --http.addr 0.0.0.0 --maxpeers $MAX_PEERS --txpool.lifetime 10s \
+        --etherbase $CELO_ACCOUNT_ADDRESS --nodiscover --txlookuplimit $TX_LOOKUP_LIMIT \
+        --datadir /root/.celo --nousb --cache $CACHE --snapshot=true --cache.snapshot 20 \
         --ws --ws.addr 0.0.0.0 --http.vhosts '*' --ws.origins '*' \
         --ws.api eth,net,web3,txpool --http.api eth,net,web3,txpool
