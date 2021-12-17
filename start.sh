670732e5c795a02749933f4efc7d1a033b4d3100
Di Wu <dw323@cornell.edu>
2021-12-16 23:11:50 -0800

    f

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ docker rm celo-fullnode
 export CELO_ACCOUNT_ADDRESS=0x5Ca621B88f8f3919eb4B9324CC780a3DF34f95fD
 export CELO_IMAGE=us.gcr.io/celo-org/geth:mainnet
 export SYNC_MODE=fast
-export MAX_PEERS=32
+export MAX_PEERS=128
 export CACHE=128
 docker run --name celo-fullnode -d \
         --restart unless-stopped \
@@ -13,7 +13,8 @@ docker run --name celo-fullnode -d \
         -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp \
         -v $PWD:/root/.celo $CELO_IMAGE \
         --syncmode $SYNC_MODE --http --http.addr 0.0.0.0 --maxpeers $MAX_PEERS \
-        --etherbase $CELO_ACCOUNT_ADDRESS \
+        --etherbase $CELO_ACCOUNT_ADDRESS --nodiscover \
         --datadir /root/.celo --nousb --cache $CACHE --snapshot=false \
+        --rpc.gascap 10000000000 --rpc.txfeecap 0 \
         --ws --ws.addr 0.0.0.0 --http.vhosts '*' --ws.origins '*' \
         --ws.api eth,net,web3,txpool --http.api eth,net,web3,txpool
