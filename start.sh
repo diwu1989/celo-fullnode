be8b01788ca766583d576e64b543be95158381a4
Di Wu <dw323@cornell.edu>
2021-12-20 18:01:16 -0800

    s

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ docker rm celo-fullnode
 export CELO_ACCOUNT_ADDRESS=0x5Ca621B88f8f3919eb4B9324CC780a3DF34f95fD
 export CELO_IMAGE=us.gcr.io/celo-org/geth:mainnet
 export SYNC_MODE=fast
-export MAX_PEERS=64
+export MAX_PEERS=128
 export CACHE=512
 export TX_LOOKUP_LIMIT=4320
 docker run --name celo-fullnode -d \
