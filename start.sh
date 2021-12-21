c033ec5cf8a146472f996d841bd94b2b39cb50fa
Di Wu <dw323@cornell.edu>
2021-12-20 18:00:53 -0800

    f

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ docker run --name celo-fullnode -d \
         -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp \
         -v $PWD:/root/.celo $CELO_IMAGE \
         --rpc.txfeecap 0 \
-        --syncmode $SYNC_MODE --http --http.addr 0.0.0.0 --maxpeers $MAX_PEERS --txpool.lifetime 10s \
+        --syncmode $SYNC_MODE --http --http.addr 0.0.0.0 --maxpeers $MAX_PEERS --txpool.lifetime 60s \
         --etherbase $CELO_ACCOUNT_ADDRESS --nodiscover --txlookuplimit $TX_LOOKUP_LIMIT \
         --datadir /root/.celo --nousb --cache $CACHE --snapshot=true --cache.snapshot 20 \
         --ws --ws.addr 0.0.0.0 --http.vhosts '*' --ws.origins '*' \
