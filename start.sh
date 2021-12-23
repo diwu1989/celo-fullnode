4b04af6da0f1b2599fef346f1129e89e2b92e059
Di Wu <dw323@cornell.edu>
2021-12-23 13:42:50 -0800

    cap peers but enable discover

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ docker rm celo-fullnode
 export CELO_ACCOUNT_ADDRESS=0x5Ca621B88f8f3919eb4B9324CC780a3DF34f95fD
 export CELO_IMAGE=us.gcr.io/celo-org/geth:mainnet
 export SYNC_MODE=fast
-export MAX_PEERS=200
+export MAX_PEERS=64
 export CACHE=512
 export TX_LOOKUP_LIMIT=4320
 docker run --name celo-fullnode -d \
@@ -13,7 +13,7 @@ docker run --name celo-fullnode -d \
         --stop-timeout 30 \
         -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp \
         -v $PWD:/root/.celo $CELO_IMAGE \
-        --rpc.txfeecap 0 --nodiscover \
+        --rpc.txfeecap 0 \
         --syncmode $SYNC_MODE --http --http.addr 0.0.0.0 --maxpeers $MAX_PEERS --txpool.lifetime 60s \
         --etherbase $CELO_ACCOUNT_ADDRESS --txlookuplimit $TX_LOOKUP_LIMIT \
         --datadir /root/.celo --nousb --cache $CACHE --snapshot=true --cache.snapshot 20 \
