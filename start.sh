1b2986f37adb2b5c34eae3e405a0ce63ee455b20
Di Wu <dw323@cornell.edu>
2022-01-26 23:42:09 -0800

    no need for etherbase

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 docker stop celo-fullnode
 docker rm celo-fullnode
 
-export CELO_ACCOUNT_ADDRESS=0x5Ca621B88f8f3919eb4B9324CC780a3DF34f95fD
 export CELO_IMAGE=us.gcr.io/celo-org/geth:mainnet
 export SYNC_MODE=fast
 export MAX_PEERS=64
@@ -15,7 +14,7 @@ docker run --name celo-fullnode -d \
         -v $PWD:/root/.celo $CELO_IMAGE \
         --rpc.txfeecap 0 \
         --syncmode $SYNC_MODE --http --http.addr 0.0.0.0 --maxpeers $MAX_PEERS --txpool.lifetime 60s \
-        --etherbase $CELO_ACCOUNT_ADDRESS --txlookuplimit $TX_LOOKUP_LIMIT \
+        --txlookuplimit $TX_LOOKUP_LIMIT \
         --datadir /root/.celo --nousb --cache $CACHE --snapshot=true --cache.snapshot 20 \
         --ws --ws.addr 0.0.0.0 --http.vhosts '*' --ws.origins '*' \
         --ws.api eth,net,web3,txpool --http.api eth,net,web3,txpool
