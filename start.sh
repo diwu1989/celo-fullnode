999d3ee8f227c19713b522a9684d862f40586169
Di Wu <dw323@cornell.edu>
2022-02-05 21:22:07 -0800

    change port

--- start.sh
+++ start.sh
@@ -7,15 +7,15 @@ export SYNC_MODE=fast
 export MAX_PEERS=64
 export CACHE=128
 export TX_LOOKUP_LIMIT=1000
+export GETH_PORT=30312
 docker run --name celo-fullnode -d \
         --restart unless-stopped \
         --stop-timeout 30 \
         --memory 3G \
-        -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp \
-        -v $PWD:/root/.celo $CELO_IMAGE \
-        --rpc.txfeecap 0 \
+        -p 8545:8545 -p 8546:8546 -p $GETH_PORT:$GETH_PORT -p $GETH_PORT:$GETH_PORT/udp \
+        -v $PWD:/root/.celo $CELO_IMAGE --port $GETH_PORT \
         --syncmode $SYNC_MODE --http --http.addr 0.0.0.0 --maxpeers $MAX_PEERS --txpool.lifetime 60s \
         --txlookuplimit $TX_LOOKUP_LIMIT \
-        --datadir /root/.celo --nousb --cache $CACHE --snapshot=true --cache.snapshot 20 \
+        --datadir /root/.celo --nousb --cache $CACHE \
         --ws --ws.addr 0.0.0.0 --http.vhosts '*' --ws.origins '*' \
         --ws.api eth,net,web3,txpool --http.api eth,net,web3,txpool
