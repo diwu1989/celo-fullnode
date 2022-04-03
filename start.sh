706abcba7b961710978d51b83ce6f8849ed9b730
Di Wu <dw323@cornell.edu>
2022-04-02 17:16:20 -0700

    less

--- start.sh
+++ start.sh
@@ -5,13 +5,13 @@ docker rm celo-fullnode
 export CELO_IMAGE=cloud.canister.io:5000/diwu1989/celo-geth:latest
 export SYNC_MODE=fast
 export MAX_PEERS=256
-export CACHE=512
+export CACHE=256
 export TX_LOOKUP_LIMIT=1000
 export GETH_PORT=30314
 docker run --name celo-fullnode -d \
         --restart unless-stopped \
         --stop-timeout 30 \
-        --memory 3G \
+        --memory 2.5G \
         -p 18545:8545 -p 18546:8546 -p $GETH_PORT:$GETH_PORT -p $GETH_PORT:$GETH_PORT/udp \
         -v $PWD:/root/.celo $CELO_IMAGE --port $GETH_PORT \
         --syncmode $SYNC_MODE --http --http.addr 0.0.0.0 --maxpeers $MAX_PEERS --txpool.lifetime 60s \
